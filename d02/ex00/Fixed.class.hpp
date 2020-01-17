e784ab314a97a231bdbbd51ac53270f63a3720a1
cameron <cameronlambert98@gmail.com>
2020-01-16 18:18:11 -0800

    ex00

--- d02/ex00/Fixed.class.hpp
+++ d02/ex00/Fixed.class.hpp
@@ -4,8 +4,23 @@
 class   Fixed
 {
     public:
-        
+        Fixed ();
+        Fixed (const Fixed &fixed);
+        Fixed & operator = (const Fixed &fixed)
+        {
+            if (this != &fixed)
+            {
+                value = fixed.value;
+            }
+            return (*this);
+        }
+        ~Fixed();
+        int                 getRawBits(void) const;
+        void                setRawBits(int const raw);
+
     private:
+        int                 value;
+        static const int    size;
 };
 
 #endif
